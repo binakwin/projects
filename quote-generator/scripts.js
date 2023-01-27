2cde548b2fd5a17c513a4b0b51864e2579c61915
Binakwin <binwin101@gmail.com>
2023-01-28 01:33:51 +0530

    final

--- quote-generator/scripts.js
+++ quote-generator/scripts.js
@@ -1,19 +1,64 @@
 // quotes from API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter-button');
+const newQ = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
 
+
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 // show new quote
 let apiQuotes = []
 function newQuote(q){
+    loading();
     // pick random quote from apiQuotes
     const quote = q[Math.floor(Math.random() * q.length)];
-    console.log(quote);
+ 
+// check author is unknown 
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    }else{
+        authorText.textContent = quote.author;
+    }
+    if (quote.text.length > 50) {
+        quoteText.classList.add('long-quote');
+        
+    } else {
+        quoteText.classList.remove('long-quote');
+        
+    }
+    quoteText.textContent = quote.text;
+   complete(); 
 }    
 
 
+    // Tweet the content
+
+
+    function tweetQuote(){
+        const tweetUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+
+        window.open(tweetUrl, '_blank');
+    }
+
+  
+    // add envent lister
+
+
 async function getQuotes(){
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
@@ -25,6 +70,10 @@ async function getQuotes(){
     }
 }
 
+newQ.addEventListener('click', getQuotes);
 
+twitterBtn.addEventListener('click', tweetQuote);
 
 getQuotes();
+
+// loading();
